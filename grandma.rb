d69000f822624efceed7493776fa6d924933707d
Brad Alexander <brad.alexander913@gmail.com>
2019-08-13 02:50:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(say)
   if say == 'I LOVE YOU GRANDMA!'
     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif say == say.upcase
+  elsif say == say.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
